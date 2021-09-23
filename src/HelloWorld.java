45b9d6710869bd257671499a894884837adf57ff
minhngavo <vonga2k2@gmail.com>
2021-09-23 07:30:29 +0700

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    
+public static void main(String[] args) {
+    System.out.println("Hế llô mụi ngừi cùng học lập trình zới mừn nhóe~~~");
+    }
+}
---
+++ src/vn/edu/giadinh/tuan02/CTMinhHoaNhapXuat.java
@@ -0,0 +1,29 @@
+package vn.edu.giadinh.tuan02;
+
+import java.util.Scanner;
+
+public class CTMinhHoaNhapXuat {
+    
+}
+public static void main(String[] args) {
+
+    System.out.print("võ" + "minh" + "nga");
+    System.out.println(2 + 2);
+
+    String tenSinhVien;
+     
+    float diemKTLapTrinh;
+     //biến tham chiếu
+    Scanner bienNhapLieu; //điều khiển từ xa
+
+    
+    bienNhapLieu = new Scanner(System.in);
+    System.out.println("Nhập vào điểm nhập môn kĩ thuật lạp trình: ");
+    diemKTLapTrinh = bienNhapLieu.nextFloat();
+
+    //ví dụ khác
+    String tenSinhVien = new String("ma mi");
+    
+
+}
+
