e8deebefb8a3fade10fdcf0f5033f8e0f86d5488
minhngavo <vonga2k2@gmail.com>
2021-12-30 21:50:55 +0700

    xinchao

---
+++ src/vn/edu/giadinh/Hello.java
@@ -0,0 +1,13 @@
+/*
+* created date: Dec 30, 2021
+* author: Võ Thị Minh Nga
+*/
+package vn.edu.giadinh;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hế llô mụi ngừi cùng học lập trình zới mừn nhóe~~~");
+        }
+    }
+    
+    
